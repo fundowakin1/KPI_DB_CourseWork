7b17b803c86221a5b8a7f4c32c1409e68af1b038
fundowakin1 <kalchenko.yehor@lll.kpi.ua>
2022-06-17 11:31:48 +0300

    created project added gitignore

---
+++ DataBasesCourseWorkApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataBasesCourseWorkApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
